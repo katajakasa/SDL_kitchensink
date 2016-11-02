c9e7f6108066d1dcbd7057794cf6f37bb03eccc5
Tuomas Virtanen <katajakasa@gmail.com>
2016-11-02 23:30:38 +0200

    Update ffmpeg to 3.0.4 in travis builder

--- travis/ffmpeg.sh
+++ travis/ffmpeg.sh
@@ -1,6 +1,6 @@
 
 if [ ! -e "$HOME/local/lib/libavcodec.so" ]; then 
-    wget https://www.ffmpeg.org/releases/ffmpeg-2.8.4.tar.gz -O ~/ffmpeg.tar.gz
+    wget https://www.ffmpeg.org/releases/ffmpeg-3.0.4.tar.gz -O ~/ffmpeg.tar.gz
     tar xzf ~/ffmpeg.tar.gz -C ~/
     cd ~/ffmpeg-2.8.4
     export CC=gcc-5
