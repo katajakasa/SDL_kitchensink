7fcc9321d498f3f9c3cb350c75bc8bbc914c10a0
Tuomas Virtanen <katajakasa@gmail.com>
2018-04-05 02:32:25 +0300

    travis: Move -y switches to start

--- travis/deps.sh
+++ travis/deps.sh
@@ -1,7 +1,7 @@
-sudo add-apt-repository ppa:zoogie/sdl2-snapshots -y
-sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y
+sudo add-apt-repository -y ppa:zoogie/sdl2-snapshots
+sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:jonathonf/ffmpeg-3
 sudo apt-get update
 sudo apt-get -y install \
     gcc-7 \
