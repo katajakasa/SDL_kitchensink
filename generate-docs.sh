96fcaacab60a582a97938f824cb872145664399b
Tuomas Virtanen <katajakasa@gmail.com>
2018-06-25 01:01:10 +0300

    Whoops

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 doxygen Doxyfile
-doxyrest docs/xml/index.xml -o doc/index.rst -F $DOXYREST_FRAME_DIR -f c_index.rst.in
+doxyrest docs/xml/index.xml -o docs/tmp/index.rst -F $DOXYREST_FRAME_DIR -f c_index.rst.in
