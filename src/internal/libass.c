4717856e01d038ec58721d3af2a063a161d2a933
Tuomas Virtanen <katajakasa@gmail.com>
2020-07-25 01:42:22 +0300

    #63: Work on GCC 10 problems

--- src/internal/libass.c
+++ src/internal/libass.c
@@ -3,6 +3,25 @@
 #include <SDL_loadso.h>
 #include "kitchensink/internal/libass.h"
 
+
+ASS_Library* (*ass_library_init)(void);
+void (*ass_library_done)(ASS_Library *priv);
+void (*ass_process_codec_private)(ASS_Track *track, char *data, int size);
+void (*ass_set_message_cb)(ASS_Library *priv, void (*msg_cb)(int level, const char *fmt, va_list args, void *data), void *data);
+ASS_Renderer* (*ass_renderer_init)(ASS_Library *);
+void (*ass_renderer_done)(ASS_Renderer *priv);
+void (*ass_set_frame_size)(ASS_Renderer *priv, int w, int h);
+void (*ass_set_hinting)(ASS_Renderer *priv, ASS_Hinting ht);
+void (*ass_set_fonts)(ASS_Renderer *priv, const char *default_font, const char *default_family, int dfp, const char *config, int update);
+ASS_Image* (*ass_render_frame)(ASS_Renderer *priv, ASS_Track *track, long long now, int *detect_change);
+ASS_Track* (*ass_new_track)(ASS_Library *);
+void (*ass_free_track)(ASS_Track *track);
+void (*ass_process_data)(ASS_Track *track, char *data, int size);
+void (*ass_process_chunk)(ASS_Track *track, char *data, int size, long long timecode, long long duration);
+void (*ass_add_font)(ASS_Library *library, char *name, char *data, int data_size);
+void (*ass_set_storage_size)(ASS_Renderer *priv, int w, int h);
+
+
 int load_libass(void *handle) {
     ass_library_init = SDL_LoadFunction(handle, "ass_library_init");
     ass_library_done = SDL_LoadFunction(handle, "ass_library_done");
