96ded6b1a42f430b93c1d59e31e447bec9786d8e
Tuomas Virtanen <katajakasa@gmail.com>
2023-10-31 01:06:07 +0200

    New, separate demuxer handler

---
+++ include/kitchensink/internal/kitbufferindex.h
@@ -0,0 +1,11 @@
+#ifndef KITBUFFERINDEX
+#define KITBUFFERINDEX
+
+typedef enum KitBufferIndex {
+    KIT_VIDEO_INDEX = 0,
+    KIT_AUDIO_INDEX,
+    KIT_SUBTITLE_INDEX,
+    KIT_INDEX_COUNT
+} KitBufferIndex;
+
+#endif // KITBUFFERINDEX
