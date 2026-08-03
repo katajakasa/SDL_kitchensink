b5bb976ecf8f1644a42fc3189c8f81f08f0d3226
Tuomas Virtanen <katajakasa@gmail.com>
2026-08-03 19:14:10 +0300

    sdl3: Clean up kiterror.c
    SDL3 automatically creates the TLS state, so we can drop some custom code here.

--- src/kiterror.c
+++ src/kiterror.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <SDL_atomic.h>
-#include <SDL_thread.h>
+#include <SDL3/SDL_atomic.h>
+#include <SDL3/SDL_thread.h>
 
 #include "kitchensink3/internal/utils/kitalloc.h"
 #include "kitchensink3/kiterror.h"
@@ -17,27 +17,17 @@ typedef struct Kit_ErrorState {
     char message[KIT_ERRBUFSIZE];
 } Kit_ErrorState;
 
-static SDL_SpinLock error_tls_lock;
-static SDL_TLSID error_tls_id = 0;
+static SDL_TLSID error_tls_id;
 
 /**
  * Fetch the error state of the current thread (TLS).
  */
 static Kit_ErrorState *Kit_GetErrorState(void) {
-    if(error_tls_id == 0) {
-        SDL_AtomicLock(&error_tls_lock);
-        if(error_tls_id == 0)
-            error_tls_id = SDL_TLSCreate();
-        SDL_AtomicUnlock(&error_tls_lock);
-        if(error_tls_id == 0)
-            return NULL;
-    }
-    Kit_ErrorState *state = SDL_TLSGet(error_tls_id);
+    Kit_ErrorState *state = SDL_GetTLS(&error_tls_id);
     if(state == NULL) {
         if((state = Kit_Calloc(1, sizeof(Kit_ErrorState))) == NULL)
             return NULL;
-        // Note: check for < 0; sdl2-compat returns 1 (SDL3 bool) on success here, real SDL2 returns 0.
-        if(SDL_TLSSet(error_tls_id, state, free) < 0) {
+        if(!SDL_SetTLS(&error_tls_id, state, free)) {
             free(state);
             return NULL;
         }
